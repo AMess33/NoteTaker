8452e6e3098b9169dc2b0aa6307f0f674e4f4ed2
AMess33 <amess3309@gmail.com>
2023-02-28 19:48:29 -0600

    add paths to server.js

--- server.js
+++ server.js
@@ -4,4 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.use(express.static('public'));
+
+// get request for the index.html page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
